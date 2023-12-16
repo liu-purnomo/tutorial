c4db40e17ad641d8deaab248d66f87f20c4f4426
liu-purnomo <kontak.liu@gmail.com>
2023-12-16 13:17:25 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Personal Style Guide 
+
+Ini adalah style guide yang saya gunakan untuk menulis kode. Style guide ini saya buat untuk memudahkan saya dalam menulis kode dan juga untuk memudahkan orang lain dalam membaca kode yang saya tulis. Style guide ini saya buat berdasarkan style guide yang sudah ada dan saya modifikasi sesuai dengan kebutuhan saya.
+
+
---
+++ theme.config.tsx
@@ -0,0 +1,85 @@
+import { useRouter } from "next/router";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+
+const logo = (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      fontSize: "1.4rem",
+      fontWeight: "bold",
+      gap: "0.5rem",
+    }}
+  >
+    <img src="/logo.png" alt="Liu Purnomo" width="30" height="30" />
+    <div className="flex">Liu Purnomo</div>
+  </div>
+);
+
+const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s – Liu Purnomo",
+      };
+    }
+  },
+  logo,
+  head: function useHead() {
+    const { title } = useConfig();
+    const { route } = useRouter();
+    const socialCard =
+      route === "/" || !title
+        ? "https://tutorial.liupurnomo.com/og.jpeg"
+        : `https://tutorial.liupurnomo.com/api/og?title=${title}`;
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="description" content="Tutorial Gratis Dari Liu Purnomo" />
+        <meta name="og:description" content="Tutorial Gratis dari Liu Purnomo" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={socialCard} />
+        <meta name="twitter:site:domain" content="tutorial.liupurnomo.com" />
+        <meta name="twitter:url" content="https://tutorial.liupurnomo.com" />
+        <meta name="og:title" content={title ? title + " – Tutorial" : "Tutorial"} />
+        <meta name="og:image" content={socialCard} />
+        <meta name="apple-mobile-web-app-title" content="tutorial" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1b2951" />
+        <meta name="msapplication-TileColor" content="#1b2951" />
+        <meta name="theme-color" content="#ffffff" />
+      </>
+    );
+  },
+  project: {
+    link: "https://github.com/liu-purnomo/tutorial",
+  },
+  chat: {
+    link: "https://discord.gg/BEeMTGmQ",
+  },
+  docsRepositoryBase: "https://gi~thub.com/liu-purnomo/style-guide/tree/main",
+  footer: {
+    text: (
+      <div className="flex w-full flex-col items-center">
+        <p className="">
+          © {new Date().getFullYear()} <a href="https://liupurnomo.com">Liu Purnomo</a>
+        </p>
+      </div>
+    ),
+  },
+  toc: {
+    backToTop: true,
+  },
+};
+
+export default config;
